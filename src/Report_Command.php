3fc0d064b4a5239dd899f6fea5d49f88d95b87b5
Nilambar Sharma <nilambar@outlook.com>
2025-08-06 15:25:59 +0545

    Add checker

--- src/Report_Command.php
+++ src/Report_Command.php
@@ -8,26 +8,131 @@
 namespace Nilambar\PCP_Report_Command;
 
 use WP_CLI;
+use WP_CLI\Utils;
 
 class Report_Command {
 
 	/**
-	 * Test command.
+	 * Generates HTML report for "plugin check" command.
 	 *
+	 * ## OPTIONS
+	 *
+	 * <plugin>
+	 * : The plugin to check.
+	 *
+	 * [--slug=<slug>]
+	 * : Slug to override the default.
+	 *
+	 * [--checks=<checks>]
+	 * : Only runs checks provided as an argument in comma-separated values.
+	 *
+	 * [--exclude-checks=<checks>]
+	 * : Exclude checks provided as an argument in comma-separated values, e.g. i18n_usage, late_escaping.
+	 * Applies after evaluating `--checks`.
+	 *
+	 * [--ignore-codes=<codes>]
+	 * : Ignore error codes provided as an argument in comma-separated values.
+	 *
+	 * [--categories]
+	 * : Limit displayed results to include only specific categories Checks.
+	 *
+	 * [--ignore-warnings]
+	 * : Limit displayed results to exclude warnings.
+	 *
+	 * [--ignore-errors]
+	 * : Limit displayed results to exclude errors.
+	 *
+	 * [--include-experimental]
+	 * : Include experimental checks.
+	 *
+	 * [--exclude-directories=<directories>]
+	 * : Additional directories to exclude from checks.
+	 * By default, `.git`, `vendor` and `node_modules` directories are excluded.
+	 *
+	 * [--exclude-files=<files>]
+	 * : Additional files to exclude from checks.
+	 *
+	 * [--severity=<severity>]
+	 * : Severity level.
+	 *
+	 * [--error-severity=<error-severity>]
+	 * : Error severity level.
+	 *
+	 * [--warning-severity=<warning-severity>]
+	 * : Warning severity level.
+	 *
+	 * [--porcelain=<field>]
+	 * : Output a single value.
+	 * ---
+	 * options:
+	 *   - path
+	 * ---
+
 	 * ## EXAMPLES
 	 *
-	 *     # Run test command.
-	 *     $ wp dashmate layout test
+	 *     # Generate report.
+	 *     $ wp pcp-report hello-dolly
 	 *
-	 * @since 1.0.0
+	 *     # Get report path only.
+	 *     $ wp pcp-report hello-dolly --porcelain=path
 	 *
-	 * @param array $args       List of the positional arguments.
-	 * @param array $assoc_args List of the associative arguments.
-	 *
-	 * @when after_wp_load
-	 * @subcommand test
+	 * @param array $args       Indexed array of positional arguments.
+	 * @param array $assoc_args Associative array of options.
 	 */
-	public function test_( $args, $assoc_args = [] ) {
-		WP_CLI::success( 'Test.' );
+	public function __invoke( $args, $assoc_args = [] ) {
+		// Keep backup.
+		$original_assoc_args = $assoc_args;
+
+		$plugin_slug = isset( $args[0] ) ? $args[0] : '';
+
+		$flags = [
+			'ignore-warnings',
+			'ignore-errors',
+			'include-experimental',
+		];
+
+		$command_text = sprintf( 'plugin check "%s"', esc_html( $plugin_slug ) );
+
+		$check_args = [
+			'format' => 'strict-json',
+			'fields' => 'file,line,column,type,code,message,docs',
+		];
+
+		$porcelain_mode = Utils\get_flag_value( $assoc_args, 'porcelain', '' );
+		unset( $assoc_args['porcelain'] );
+
+		$check_args = array_merge( $assoc_args, $check_args );
+
+		foreach ( $check_args as $key => $val ) {
+			if ( in_array( $key, $flags, true ) ) {
+				$command_text .= " --{$key}";
+			} else {
+				$command_text .= " --{$key}={$val}";
+			}
+		}
+
+		$result_obj = WP_CLI::runcommand(
+			$command_text,
+			[
+				'return'     => 'all',
+				'launch'     => true,
+				'exit_error' => false,
+			]
+		);
+
+		// Error occurred.
+		if ( 1 === $result_obj->return_code ) {
+			WP_CLI::line( $result_obj->stderr );
+			WP_CLI::halt( 1 );
+		}
+
+		$stdout = $result_obj->stdout;
+
+		if ( empty( $stdout ) || str_contains( $stdout, 'Checks complete. No errors found.' ) ) {
+			WP_CLI::success( 'No errors or warnings.' );
+			return;
+		}
+
+		WP_CLI::line( $stdout );
 	}
 }
